fb2885cf14ff47dec3c0e9154d73c4ec715f1f13
kroltan <kroltan1@gmail.com>
2015-09-05 01:16:31 -0300

    close button

---
+++ Assets/Exosphir/Scripts/Interface/WindowClose.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Interface {
+    [RequireComponent(typeof(Button))]
+    public class WindowClose : MonoBehaviour {
+        public RectTransform Window;
+
+        void Start () {
+            var button = GetComponent<Button>();
+            button.onClick.AddListener(Close);
+        }
+
+        private void Close() {
+            Window.gameObject.SetActive(false);
+        }
+    }
+}
